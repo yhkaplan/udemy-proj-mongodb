5b88f7ba88a7cfe9667855331b61036fdeb2cbdc
josh <josh@pepabo.com>
2018-11-28 13:57:26 +0900

    Add createUser and deleteUser

--- main.go
+++ main.go
@@ -13,11 +13,29 @@ func main() {
 	r := httprouter.New()
 	r.GET("/", index)
 	r.GET("/user/:id", getUser)
+	r.POST("/user", createUser)
+	r.DELETE("/user/:id", deleteUser)
 	http.ListenAndServe("localhost:8080", r)
 }
 
 func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprint(w, "Welcome!\n")
+}
+
+func createUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := model.User{}
+	json.NewDecoder(r.Body).Decode(&u)
+	u.Id = "007"
+	uj, _ := json.Marshal(u)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "Write code to delete user")
 }
 
 func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
